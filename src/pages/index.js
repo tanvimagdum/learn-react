442948fb41668707a5270410004c677fd5e1458b
tanvimagdum <magdum.t@northeastern.edu>
2024-02-15 15:08:00 -0500

    first

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+import List from './qcomps/list_keys_id'
 
 export default function Home() {
   return (
     <div className={styles.main}>
         <Profile />
+        <List/>
     </div>
   )
 }
